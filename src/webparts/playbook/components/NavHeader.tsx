5fe7a68c367d3bf594de8f36d4e0af8680c4bc01
AbduSalam08 <asa882003@gmail.com>
2022-09-05 22:44:15 +0530

    Salam Design Changes

--- src/webparts/playbook/components/NavHeader.tsx
+++ src/webparts/playbook/components/NavHeader.tsx
@@ -5,11 +5,40 @@ import { useState, useEffect } from "react";
 import { Icon } from "@fluentui/react";
 const headerLogo = require("./../../../ExternalRef/img/GGSATopLogo.png");
 const menu = require("./../../../ExternalRef/img/menu.png");
+import { Panel } from "@fluentui/react/lib/Panel";
+import { useBoolean } from "@fluentui/react-hooks";
+
 const NavHeader = () => {
+  const [isOpen, { setTrue: openPanel, setFalse: dismissPanel }] =
+    useBoolean(false);
+
   return (
-    <div className={styles.headerSection}>
-      <img src={`${headerLogo}`} alt="logo" />
-      <img className={styles.navMenu} src={`${menu}`} alt="menu" />
+    <div>
+      <div className={styles.headerSection}>
+        <img src={`${headerLogo}`} alt="logo" />
+        <img
+          className={styles.navMenu}
+          src={`${menu}`}
+          alt="menu"
+          onClick={openPanel}
+        />
+      </div>
+      <div>
+        <Panel
+          isLightDismiss
+          isOpen={isOpen}
+          onDismiss={dismissPanel}
+          closeButtonAriaLabel="Close"
+          headerText="Menu"
+        >
+          <div className={styles.navWrapper}>
+            <div className={styles.linkActive}>Phases</div>
+            <div className={styles.navbtn}>Practice</div>
+            <div className={styles.navbtn}>Pathway</div>
+            <div className={styles.navbtn}>HelpGuid</div>
+          </div>
+        </Panel>
+      </div>
     </div>
   );
 };
